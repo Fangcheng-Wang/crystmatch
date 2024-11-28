29a87cd61cb9117906c5c1dd9901ed78be669d5a
Fangcheng-Wang <wfc@pku.edu.cn>
2024-11-28 23:29:48 +0800

    Deleting 'build' folder

--- build/lib/crystmatch/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-A Python package for exhausting crystal-structure matches for solid-solid phase transitions.
-"""
-
-from .io import *
-from .core import *
-from .analysis import *
-from .cli import *
